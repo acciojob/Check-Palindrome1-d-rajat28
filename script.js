f6baa143224bf5eec6349229cbbbb5b324f51ca7
Rajat Deb <109984521+d-rajat28@users.noreply.github.com>
2023-03-16 20:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-return str === str.split("").reverse().join("");
+var rev_str = "";
+      for( var i = str.length-1 ;i >= 0 ;i--)
+      {
+        rev_str+= str[i];
+      }
+	if(str === rev_str){
+		return true;
+	}else{
+		return  false;
+	}
 }
 module.exports = palindrome
